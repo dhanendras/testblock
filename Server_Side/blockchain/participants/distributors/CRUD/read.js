ef1a7ddc244b8aca29460e14cc6ce4f408a4fba3
Dhanendra <dhanendra.chaudhary@gmail.com>
2017-03-19 15:22:37 +0530

    f

--- Server_Side/blockchain/participants/distributors/CRUD/read.js
+++ Server_Side/blockchain/participants/distributors/CRUD/read.js
@@ -1,10 +1,18 @@
-'use strict';
 
 let tracing = require(__dirname+'/../../../../tools/traces/trace.js');
-let participants = require(__dirname+'/../../participants_info.js');
+var reload = require('require-reload')(require),
+    participants = reload(__dirname+'/../../participants_info.js');
+
 
 let read = function(req, res)
 {
+participants = reload(__dirname+'/../../participants_info.js');
+	tracing.create('ENTER', 'GET blockchain/participants/distributors', {});
+		for (var key in participants) {
+  
+    console.log(key + " -> " );
+  
+}
     tracing.create('ENTER', 'GET blockchain/participants/distributors', {});
 
     if(!participants.hasOwnProperty('distributors'))
--- Server_Side/blockchain/participants/distributors/distributors.js
+++ Server_Side/blockchain/participants/distributors/distributors.js
@@ -1,4 +1,7 @@
 /*eslint-env node */
+console.log("distributor==================");
+console.trace("distrdibutor==================");
 var read = require(__dirname+'/CRUD/read.js');
 console.log("distributor=================="+read);
+console.trace("distribuetor=================="+read);
 exports.read = read.read;
