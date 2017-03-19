2eaa55c083a1d65e270bed7b58a2dfa77a17ed31
Dhanendra <dhanendra.chaudhary@gmail.com>
2017-03-19 18:47:50 +0530

    l

--- Server_Side/blockchain/participants/distributors/distributors.js
+++ Server_Side/blockchain/participants/distributors/distributors.js
@@ -1,5 +1,3 @@
 /*eslint-env node */
 var read = require(__dirname+'/CRUD/read.js');
-tracing.create('ENTER', 'GET blockchain/participants/distributors', read);
-
-//exports.read = read.read;
+exports.read = read.read;
