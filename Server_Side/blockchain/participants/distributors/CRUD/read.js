71a19762c00c6bb3618a589c5db0cc46c9fbfd3e
Dhanendra <dhanendra.chaudhary@gmail.com>
2017-03-19 19:04:59 +0530

    f

--- Server_Side/blockchain/participants/distributors/CRUD/read.js
+++ Server_Side/blockchain/participants/distributors/CRUD/read.js
@@ -1,25 +1,31 @@
-'use strict';
+/*eslint-env node */
 
-let tracing = require(__dirname+'/../../../../tools/traces/trace.js');
-let participants = require(__dirname+'/../../participants_info.js');
+var tracing = require(__dirname+'/../../../../tools/traces/trace.js');
+var reload = require('require-reload')(require),
+    participants = reload(__dirname+'/../../participants_info.js');
 
-let read = function(req, res)
+var read = function(req, res)
 {
-    tracing.create('ENTER', 'GET blockchain/participants/distributors', {});
-
-    if(!participants.participants_info.hasOwnProperty('distributors'))
+	participants = reload(__dirname+'/../../participants_info.js');
+	tracing.create('ENTER', 'GET blockchain/participants/miners', {});
+		for (var key in participants.participants_info) {
+  
+    console.log(key + " -> " +participants.participants_info[key]);
+  
+}
+	if(!participants.participants_info.hasOwnProperty('miners'))
 	{
-        res.status(404);
-        let error = {};
-        error.message = 'Unable to retrieve distributors';
-        error.error = true;
-        tracing.create('ERROR', 'GET blockchain/participants/distributors', error);
-        res.send(error);
-    }
-    else
+		res.status(404)
+		var error = {}
+		error.message = 'Unable to retrieve miners'
+		error.error = true;
+		tracing.create('ERROR', 'GET blockchain/participants/miners', error);
+		res.send(error)
+	} 
+	else
 	{
-        tracing.create('EXIT', 'GET blockchain/participants/distributors', {'result':participants.participants_info.distributors});
-        res.send({'result':participants.participants_info.distributors});
-    }
-};
+		tracing.create('EXIT', 'GET blockchain/participants/miners', {"result":participants.participants_info.miners});
+		res.send({"result":participants.participants_info.miners})
+	}
+}
 exports.read = read;
