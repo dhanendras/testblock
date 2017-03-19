f33506279b9021b38e9a86dc270bc6c7ec4cf573
Dhanendra <dhanendra.chaudhary@gmail.com>
2017-03-19 17:20:19 +0530

    f

--- Server_Side/blockchain/participants/distributors/CRUD/read.js
+++ Server_Side/blockchain/participants/distributors/CRUD/read.js
@@ -1,31 +1,26 @@
-/*eslint-env node */
+'use strict';
 
-var tracing = require(__dirname+'/../../../../tools/traces/trace.js');
-var reload = require('require-reload')(require),
-    participants = reload(__dirname+'/../../participants_info.js');
+let tracing = require(__dirname+'/../../../../tools/traces/trace.js');
+let participants = require(__dirname+'/../../participants_info.js');
 
-var read = function(req, res)
+let read = function(req, res)
 {
-	participants = reload(__dirname+'/../../participants_info.js');
-	tracing.create('ENTER', 'GET blockchain/participants/miners', {});
-		for (var key in participants) {
-  
-    console.log(key + " -> " );
-  
-}
-	if(!participants.participants_info.hasOwnProperty('miners'))
-	{
-		res.status(404)
-		var error = {}
-		error.message = 'Unable to retrieve miners'
-		error.error = true;
-		tracing.create('ERROR', 'GET blockchain/participants/miners', error);
-		res.send(error)
-	} 
-	else
-	{
-		tracing.create('EXIT', 'GET blockchain/participants/miners', {"result":participants.participants_info.miners});
-		res.send({"result":participants.participants_info.miners})
-	}
-}
+    tracing.create('ENTER', 'GET blockchain/participants/distributors', {});
+
+    if(!participants.participants_info.hasOwnProperty('distributors'))
+    {
+        res.status(404);
+        let error = {};
+        error.message = 'Unable to retrieve distributors';
+        error.error = true;
+        tracing.create('ERROR', 'GET blockchain/participants/distributors', error);
+        res.send(error);
+    }
+    else
+    {
+        tracing.create('EXIT', 'GET blockchain/participants/distributors', {'result':participants.participants_info.distributors});
+        res.send({'result':participants.participants_info.distributors});
+    }
+
+};
 exports.read = read;
